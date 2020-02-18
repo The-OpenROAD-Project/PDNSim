d7fb1fc9bd6e8f86645417d31e28357c19988fb2
VidyaChhabria <chhab011@umn.edu>
2020-02-18 12:18:54 -0600

    Update Dockerfile

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,2 +1,2 @@
 docker build -f jenkins/Dockerfile.dev -t pdnsim .
-docker run -v -it $(pwd):/PDNSim pdnsim bash -c "./PDNSim/jenkins/install.sh"
+docker run -v -u $(id -u ${USER}):$(id -g ${USER}) -it $(pwd):/PDNSim pdnsim bash -c "./PDNSim/jenkins/install.sh"
