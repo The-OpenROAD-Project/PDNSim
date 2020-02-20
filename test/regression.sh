72118ffb7147998a93f0734184915f6c260e7a88
VidyaChhabria <chhab011@umn.edu>
2020-02-19 22:36:15 -0600

    Fixing test

--- test/regression.sh
+++ test/regression.sh
@@ -1,8 +1,10 @@
 echo "Running tests .."
 echo "========================="
 
+APP=./PDNSim/build/pdnsim
+
 echo "======== TEST 1: ========"
-./pdnsim --input-lef ./PDNSim/test/nangate45/Nangate45.lef --input-def \
+$APP --input-lef ./PDNSim/test/nangate45/Nangate45.lef --input-def \
 ./PDNSim/test/gcd/gcd.def --input-lib \
 ./PDNSim/test/nangate45/NangateOpenCellLibrary_typical.lib --input-verilog \
 ./PDNSim/test/gcd/gcd.v --input-sdc ./PDNSim/test/gcd/gcd.sdc --input-vsrc \
@@ -24,7 +26,7 @@ fi
 echo "========================="
 
 echo "======== TEST 2: ========"
-./pdnsim --input-lef ../test/nangate45/Nangate45.lef --input-def \
+$APP --input-lef ../test/nangate45/Nangate45.lef --input-def \
 ./PDNSim/test/aes/aes.def --input-lib \
 ./PDNSim/test/nangate45/NangateOpenCellLibrary_typical.lib --input-verilog \
 ./PDNSim/test/aes/aes.v --input-sdc ./PDNSim/test/aes/aes.sdc --input-vsrc \
