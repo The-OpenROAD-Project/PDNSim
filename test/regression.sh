08a5ffea244af9afcbe009122d2a8e47dde8dffe
VidyaChhabria <chhab011@umn.edu>
2020-02-19 23:11:22 -0600

    Fixing tests

--- test/regression.sh
+++ test/regression.sh
@@ -8,7 +8,7 @@ $APP --input-lef ./PDNSim/test/nangate45/Nangate45.lef --input-def \
 ./PDNSim/test/gcd/gcd.def --input-lib \
 ./PDNSim/test/nangate45/NangateOpenCellLibrary_typical.lib --input-verilog \
 ./PDNSim/test/gcd/gcd.v --input-sdc ./PDNSim/test/gcd/gcd.sdc --input-vsrc \
-./PDNSim/test/gcd/Vsrc.loc --top-module gcd > ./PDNSim/test/gcd/gcd_test.check 
+./PDNSim/test/gcd/Vsrc.loc --top-module gcd | tee ./PDNSim/test/gcd/gcd_test.check 
 
 diff --brief ./PDNSim/test/gcd/gcd_test.check ./PDNSim/test/gcd/gcd_test.ok >/dev/null
 comp_value=$?
@@ -16,8 +16,8 @@ comp_value=$?
 if [ $comp_value -eq 1 ]
 then
     echo "FAIL!"
-    exit 1
     echo "===== TEST 1: FAIL ======"
+    exit 1
 else
     echo "SUCCESS!"
     echo "===== TEST 1: PASS ======"
@@ -26,11 +26,11 @@ fi
 echo "========================="
 
 echo "======== TEST 2: ========"
-$APP --input-lef ../test/nangate45/Nangate45.lef --input-def \
+$APP --input-lef ./PDNSim/test/nangate45/Nangate45.lef --input-def \
 ./PDNSim/test/aes/aes.def --input-lib \
 ./PDNSim/test/nangate45/NangateOpenCellLibrary_typical.lib --input-verilog \
 ./PDNSim/test/aes/aes.v --input-sdc ./PDNSim/test/aes/aes.sdc --input-vsrc \
-./PDNSim/test/aes/Vsrc.loc --top-module aes_cipher_top > ./PDNSim/test/aes/aes_test.check 
+./PDNSim/test/aes/Vsrc.loc --top-module aes_cipher_top | tee ./PDNSim/test/aes/aes_test.check 
 
 diff --brief ./PDNSim/test/aes/aes_test.check ./PDNSim/test/aes/aes_test.ok >/dev/null
 comp_value=$?
