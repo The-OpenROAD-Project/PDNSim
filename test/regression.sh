09f8551ebe15d39f9345ac1f685978490da4b3a7
VidyaChhabria <chhab011@umn.edu>
2020-02-19 22:24:09 -0600

    Fixing test

--- test/regression.sh
+++ test/regression.sh
@@ -2,18 +2,19 @@ echo "Running tests .."
 echo "========================="
 
 echo "======== TEST 1: ========"
-./pdnsim --input-lef ../test/nangate45/Nangate45.lef --input-def \
-../test/gcd/gcd.def --input-lib \
-../test/nangate45/NangateOpenCellLibrary_typical.lib --input-verilog \
-../test/gcd/gcd.v --input-sdc ../test/gcd/gcd.sdc --input-vsrc \
-../test/gcd/Vsrc.loc --top-module gcd > ../test/gcd/gcd_test.check 
+./pdnsim --input-lef ./PDNSim/test/nangate45/Nangate45.lef --input-def \
+./PDNSim/test/gcd/gcd.def --input-lib \
+./PDNSim/test/nangate45/NangateOpenCellLibrary_typical.lib --input-verilog \
+./PDNSim/test/gcd/gcd.v --input-sdc ./PDNSim/test/gcd/gcd.sdc --input-vsrc \
+./PDNSim/test/gcd/Vsrc.loc --top-module gcd > ./PDNSim/test/gcd/gcd_test.check 
 
-diff --brief ../test/gcd/gcd_test.check ../test/gcd/gcd_test.ok >/dev/null
+diff --brief ./PDNSim/test/gcd/gcd_test.check ./PDNSim/test/gcd/gcd_test.ok >/dev/null
 comp_value=$?
 
 if [ $comp_value -eq 1 ]
 then
     echo "FAIL!"
+    exit 1
     echo "===== TEST 1: FAIL ======"
 else
     echo "SUCCESS!"
@@ -24,18 +25,19 @@ echo "========================="
 
 echo "======== TEST 2: ========"
 ./pdnsim --input-lef ../test/nangate45/Nangate45.lef --input-def \
-../test/aes/aes.def --input-lib \
-../test/nangate45/NangateOpenCellLibrary_typical.lib --input-verilog \
-../test/aes/aes.v --input-sdc ../test/aes/aes.sdc --input-vsrc \
-../test/aes/Vsrc.loc --top-module aes_cipher_top > ../test/aes/aes_test.check 
+./PDNSim/test/aes/aes.def --input-lib \
+./PDNSim/test/nangate45/NangateOpenCellLibrary_typical.lib --input-verilog \
+./PDNSim/test/aes/aes.v --input-sdc ./PDNSim/test/aes/aes.sdc --input-vsrc \
+./PDNSim/test/aes/Vsrc.loc --top-module aes_cipher_top > ./PDNSim/test/aes/aes_test.check 
 
-diff --brief ../test/aes/aes_test.check ../test/aes/aes_test.ok >/dev/null
+diff --brief ./PDNSim/test/aes/aes_test.check ./PDNSim/test/aes/aes_test.ok >/dev/null
 comp_value=$?
 
 if [ $comp_value -eq 1 ]
 then
     echo "FAIL!"
     echo "===== TEST 2: FAIL ======"
+    exit 1
 else
     echo "SUCCESS!"
     echo "===== TEST 2: PASS ======"
